f1d67ba3f0118c5200098af22099097676dfa357
zkaedii <ideakzkaedi@outlook.com>
2026-04-21 13:25:16 -0700

    fix: jmp_buf 200 bytes (long[25]) on x86-64 glibc - was causing stack corruption in setjmp/longjmp

--- zcc_sys_includes/setjmp.h
+++ zcc_sys_includes/setjmp.h
@@ -1,6 +1,9 @@
 #ifndef _SETJMP_H
 #define _SETJMP_H
-typedef long jmp_buf[32];
+/* x86-64 glibc: sizeof(jmp_buf) = 200 bytes
+ * = __jmp_buf(long[8]=64) + __mask_was_saved(int=4) + __sigset_t(128) + pad(4)
+ * Use long[25] = 200 bytes to match. */
+typedef long jmp_buf[25];
 int  setjmp(jmp_buf env);
 void longjmp(jmp_buf env, int val);
 #endif
